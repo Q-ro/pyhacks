e1ae6ccd1427fd38bf631f019ce90683fe5e4f76
q-ro <andres.mrad@gmail.com>
2017-05-25 20:07:02 +0100

    testing promps

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hacks")
+###
+# author : Andres Mrad (Q-ro)
+# Description : A testing ground for python
+###
+name  = (input('Enter name : '))
+
+if len(name) > 4:
+	print('wow, that\'s a long name')
+else:
+	print('that\'s my type of person, nice')
+
