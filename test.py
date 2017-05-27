69bb2f0028c86daa0065495694c612ac35bb619e
q-ro <andres.mrad@gmail.com>
2017-05-27 19:16:30 +0100

    a guess which number im thinking game

--- test.py
+++ test.py
@@ -1,11 +1,64 @@
 ###
 # author : Andres Mrad (Q-ro)
-# Description : A testing ground for python
+# Description : A clompletely pointless guessing game
 ###
-name  = (input('Enter name : '))
+import random
 
-if len(name) > 4:
-	print('wow, that\'s a long name')
+def PickNumber(ramgeStart, rangeEnd):
+	return random.randint(rangeStart,rangeEnd)
+
+def ValidateDifficulty ():
+	validDifficulty = False
+	print('''Select difficulty :\n
+1) Noob.\n
+2) Human.\n
+3) Hacker.\n
+4) Impossible.\n ''')
+	while not validDifficulty:
+		try:
+			selection  = int(input('Enter difficulty: '))
+		except ValueError:
+			print('That\'s not even a number')
+			continue
+		else:
+			if selection >= 1 and selection <=4:
+				validDifficulty = True
+				return selection
+			else:
+				print('That is not a valid option')
+
+def PrintGreet():
+	print('''Hello, welcome to number guesser,
+it\'s good that you want to waste time.\n''')
+
+def GuessNumber(number):
+	gameNotOver = False
+	while gameNotOver:
+		try:
+			guess = int(input('Tell me your guess')
+		except ValueError:
+			print('That\'s not even a number'
+		else:
+			if guess == number:
+				print('You did it !')
+
+guessing = True
+guessedCorrectly = False
+chosenOne = False
+numberToGuess = 0
+
+PrintGreet()
+difficulty = ValidateDifficulty()
+
+if difficulty == 1:
+	numberToGuess = PickNumber(0,3)
+elif difficulty == 2:
+	numberToGuess = PickNumber(0,10)
+elif difficulty == 3:
+	numberToGuess = PickRange(0,100)
+elif difficulty == 4:
+	numberToGuess = PickRange(0,100000000)
 else:
-	print('that\'s my type of person, nice')
+	print('''I don\'t know how you did it, but this is not a valid 
+option, to the corn field with you !''')
 
