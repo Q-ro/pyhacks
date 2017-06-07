21dbbc4c243c6b6b825ba121ceb4231fbeefd849
Andres Mrad (Q-ro) <andres.mrad@gmail.com>
2017-06-07 05:42:30 +0100

    The game is done
    WEll, not a super complex game but it has all the functionality i set up
    to implement, so, yay ! (?)

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 # Description : A clompletely pointless guessing game
 ###
 import random
+import sys
 
 def PickNumber(rangeStart, rangeEnd):
 	return random.randint(rangeStart,rangeEnd)
@@ -13,7 +14,8 @@ def ValidateDifficulty ():
 1) Noob.\n
 2) Human.\n
 3) Hacker.\n
-4) Impossible.\n ''')
+4) Impossible.\n
+5) Exit. \n''')
 	while not validDifficulty:
 		try:
 			selection  = int(input('Enter difficulty: '))
@@ -24,6 +26,8 @@ def ValidateDifficulty ():
 			if selection >= 1 and selection <=4:
 				validDifficulty = True
 				return selection
+			elif selection == 5:
+				sys.exit("see you again !")
 			else:
 				print('That is not a valid option')
 
@@ -32,40 +36,44 @@ def PrintGreet():
 it\'s good that you want to waste time.\n''')
 
 def GuessNumber(number):
-	gameNotOver = False
-	while not gameNotOver:
+	gameOver = False
+	while not gameOver:
 		try:
-			guess = int(input('Tell me your guess'))
+			guess = int(input('Tell me your guess : '))
 		except ValueError:
 			print('That\'s not even a number')
 			continue
 		else:
 			if guess == number:
 				print('You did it !')
-				gameNotOver = True
+				gameOver = True
 			elif guess > number:
 				print('The number is smaller')
 			elif guess < number:
 				print('The number is bigger')
 
-guessing = True
-guessedCorrectly = False
+endGame = False
 chosenOne = False
 numberToGuess = 0
 
 PrintGreet()
-difficulty = ValidateDifficulty()
+while not endGame:
+    difficulty = ValidateDifficulty()
 
-if difficulty == 1:
-	numberToGuess = PickNumber(0,3)
-elif difficulty == 2:
-	numberToGuess = PickNumber(0,10)
-elif difficulty == 3:
-	numberToGuess = PickRange(0,100)
-elif difficulty == 4:
-	numberToGuess = PickRange(0,100000000)
-else:
-	print('''I don\'t know how you did it, but this is not a valid 
-option, to the corn field with you !''')
+    if difficulty == 1:
+    	numberToGuess = PickNumber(0,3)
+    elif difficulty == 2:
+    	numberToGuess = PickNumber(0,10)
+    elif difficulty == 3:
+    	numberToGuess = PickNumber(0,100)
+    elif difficulty == 4:
+    	numberToGuess = PickNumber(0,100000000)
+    	chosenOne  = True
+    else:
+    	print('''I don\'t know how you did it, but this is not a valid
+    option, to the corn field with you !''')
 
-GuessNumber(numberToGuess)
+    GuessNumber(numberToGuess)
+    if chosenOne:
+        print("Holy cow you are the chosen one !!!!! \n Don\'t waste your time with this silly game, go and cure cancer or something !")
+        sys.exit()
